dfe4bcd389c08dd7e3abbf85fa24203767f3fd93
BoyPhanna <kimphanna56@gmail.com>
2025-01-16 15:04:40 +0700

    u

---
+++ index.js
@@ -0,0 +1,32 @@
+// server.js
+const express = require("express");
+const User = require("./models/user");
+
+const app = express();
+app.use(express.json());
+
+// Create a new user
+app.post("/users", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// Get all users
+app.get("/users", async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
